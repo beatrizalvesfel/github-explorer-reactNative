a3ea325ba00add3c86feb3345a291d5d0570342a
beatrizalvesfel <beatrizalvesfel@gmail.com>
2021-02-06 16:07:41 -0300

    configurações iniciais

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Stack = createStackNavigator();
+
+function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: '#ff0057' },
+          headerTitleAlign: 'center',
+          headerTintColor: '#FFF',
+          headerBackTitleVisible: false,
+        }}
+      >
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{ title: 'Usuários' }}
+        />
+        <Stack.Screen name="User" component={User} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Routes;
